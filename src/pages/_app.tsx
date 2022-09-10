daae55de4f2b978eb13802478fa00e348a3ef841
Habeeb Ahmadu <habeebahmadu1@gmail.com>
2022-09-10 16:10:04 +0100

    Updated my google client id

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,22 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import { ThemeProvider } from 'next-themes';
-import NextNProgress from 'nextjs-progressbar';
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import { Toaster } from 'react-hot-toast';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "next-themes";
+import NextNProgress from "nextjs-progressbar";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { Toaster } from "react-hot-toast";
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return (
-		<GoogleOAuthProvider
-			clientId={`${process.env.NEXT_PUBLIC_GOOGLE_OAUTH_CLIENTID}`}
-		>
-			<ThemeProvider attribute="class">
-				<NextNProgress />
-				<Toaster />
-				<Component {...pageProps} />
-			</ThemeProvider>
-		</GoogleOAuthProvider>
-	);
+  return (
+    <GoogleOAuthProvider
+      clientId={`${process.env.NEXT_PUBLIC_GOOGLE_OAUTH_CLIENT_ID}`}
+    >
+      <ThemeProvider attribute="class">
+        <NextNProgress />
+        <Toaster />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </GoogleOAuthProvider>
+  );
 }
 
 export default MyApp;
