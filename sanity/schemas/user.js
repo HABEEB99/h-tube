3fc115e8571fc8514281d8a829d09fa11e789a16
Habeeb Ahmadu <habeebahmadu1@gmail.com>
2022-09-11 19:02:32 +0100

    Updated User schema in Sanity

--- sanity/schemas/user.js
+++ sanity/schemas/user.js
@@ -1,27 +1,32 @@
 export default {
-	name: 'user',
-	title: 'User',
-	type: 'document',
-	fields: [
-		{
-			name: 'userName',
-			title: 'User Name',
-			type: 'string',
-		},
-		{
-			name: 'fullName',
-			title: 'Full Name',
-			type: 'string',
-		},
-		{
-			name: 'email',
-			title: 'Email',
-			type: 'string',
-		},
-		{
-			name: 'picture',
-			title: 'Picture',
-			type: 'string',
-		},
-	],
+  name: "user",
+  title: "User",
+  type: "document",
+  fields: [
+    {
+      name: "userName",
+      title: "User Name",
+      type: "string",
+    },
+    {
+      name: "fullName",
+      title: "Full Name",
+      type: "string",
+    },
+    {
+      name: "email",
+      title: "Email",
+      type: "string",
+    },
+    {
+      name: "picture",
+      title: "Picture",
+      type: "string",
+    },
+    {
+      name: "isAdmin",
+      title: "Is Admin",
+      type: "boolean",
+    },
+  ],
 };
